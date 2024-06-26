03cc67e8f3bb374d311daa5b853f3f744baf6eb2
GhazalADel <gashoftedel@aut.ac.ir>
2024-06-27 01:00:08 +0330

    feat : template and url

--- src/group14/models.py
+++ src/group14/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Card(models.Model):
+    id = models.AutoField(primary_key=True)
+    front_value = models.CharField(max_length=255)
+    back_value = models.CharField(max_length=255)
+    box_number = models.IntegerField()
+    # user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    last_review = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
--- src/group14/views.py
+++ src/group14/views.py
@@ -1,3 +1,10 @@
+# group14/views.py
+
 from django.shortcuts import render
+from django.http import HttpResponse
+from database.query import create_db_connection, get_posts_by_user_id
+from database.secret import DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME
 
 # Create your views here.
+def home(request):
+    return render(request, 'group14/home.html')
