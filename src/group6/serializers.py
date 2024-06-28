d12f0079558fc24944e77eb85eb6eb7066c95c39
Adibov <hesamadibi80@gmail.com>
2024-06-28 19:53:03 +0330

    feat: implement chat room APIs

--- src/group6/serializers.py
+++ src/group6/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import FAQ
+from .models import FAQ, Chat, UserChat, Message
 
 
 class FAQSerializer(serializers.ModelSerializer):
@@ -11,3 +11,23 @@ class FAQSerializer(serializers.ModelSerializer):
 
     def get_is_leaf(self, obj):
         return obj.get_children() == []
+
+
+class ChatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Chat
+        fields = ['id']
+
+
+class UserChatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserChat
+        fields = ['id', 'user', 'chat']
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = serializers.StringRelatedField()  # To get a string representation of the user
+
+    class Meta:
+        model = Message
+        fields = ['id', 'chat', 'text', 'sender', 'timestamp']
