edea162a52e93a3fb3b5ccd30afe8e160525077f
Mahyar Esmaeeli <mah8345es@gmail.com>
2024-06-29 17:42:08 +0330

    feat : added data transfer objects

---
+++ src/group10/Teacher-Service-API/Teacher-Service-API/Models/DTOs/CourseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Teacher_Service_API.Models.DTOs
+{
+    public class CourseDTO(string id, string description, string name, string objectives)
+    {        
+
+        public string Id { get; set; } = id;
+        public string Description { get; set; } = description;
+        public string Name { get; set; } = name;
+        public string Objectives { get; set; } = objectives;
+    }
+}
