57ae358c72c029208bc382a94b63bf53a25b8d01
mehdinajibpour <najibpour6@gmail.com>
2024-06-29 15:38:59 +0330

    make models for category, part, word

--- src/group5/models.py
+++ src/group5/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Part(models.Model):
+    category = models.ForeignKey(Category, related_name='parts', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Word(models.Model):
+    part = models.ForeignKey(Part, related_name='words', on_delete=models.CASCADE)
+    exact_word = models.CharField(max_length=100)
+    definition = models.TextField()
+    image = models.ImageField(upload_to='word_images/')
+
+    def __str__(self):
+        return self.exact_word
