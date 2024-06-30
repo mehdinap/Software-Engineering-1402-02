deae11a01d8bec4bdeba979ff40ae72fe5edc487
Shakir <shakir@gmail.com>
2024-06-30 22:05:03 +0330

    initial commit

--- src/group15/models.py
+++ src/group15/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class ListeningQuestion(models.Model):
+    audio_file = models.FileField(upload_to='audio/')
+    question_text = models.TextField()
+    correct_answer = models.TextField()
+
+class ReadingQuestion(models.Model):
+    passage_text = models.TextField()
+    question_text = models.TextField()
+    correct_answer = models.TextField()
+
+class WritingTask(models.Model):
+    task_number = models.IntegerField()
+    question_text = models.TextField()
+
+class SpeakingQuestion(models.Model):
+    PART_CHOICES = [
+        (1, 'Part 1'),
+        (2, 'Part 2'),
+        (3, 'Part 3'),
+    ]
+    part = models.IntegerField(choices=PART_CHOICES, default=1)
+    question_text = models.TextField()
+    def __str__(self):
+        return f"Part {self.part}: {self.question_text}"
--- src/group15/urls.py
+++ src/group15/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
-
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('listening/', views.listening_test, name='listening_test'),
+    path('submit_listening/', views.submit_listening_test, name='submit_listening_test'),
+    path('reading/', views.reading_test, name='reading_test'),
+    path('reading/', views.reading_test, name='reading_test'),
+    path('submit_reading/', views.submit_reading_test, name='submit_reading_test'),
+    path('writing/part1/', views.writing_test_part1, name='writing_test_part1'),
+    path('writing/part2/', views.writing_test_part2, name='writing_test_part2'),
+    path('', views.home, name='home'),
+    path('speaking/', views.speaking_test, name='speaking_test'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-] 
