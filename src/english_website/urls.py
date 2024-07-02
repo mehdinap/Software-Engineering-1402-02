134a2dc55322265941a878b14c539bf75bfb32ac
mehdinajibpour <najibpour6@gmail.com>
2024-07-03 00:17:06 +0330

    fix

--- src/english_website/urls.py
+++ src/english_website/urls.py
@@ -19,6 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -40,4 +42,4 @@ urlpatterns = [
     path("group15/", include("group15.urls")),
     path("group16/", include("group16.urls")),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
